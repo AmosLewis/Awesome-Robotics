fca061dae688f90c940daa931744accabc8ae629
Chi_Liu <chil1@alumni.cmu.edu>
2018-06-04 19:55:32 -0400

    Create binary_search.cpp

---
+++ C_plusplus/leetcode/binary_search.cpp
@@ -0,0 +1,50 @@
+class Solution 
+{
+public:
+    /**
+     * 159. Find Minimum in Rotated Sorted Array
+     * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+     * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+     *  Find the minimum element.
+     * Example
+     * Given [4, 5, 6, 7, 0, 1, 2] return 0
+     */
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    int findMin(vector<int> &nums) 
+    {
+        // write your code here
+        if (nums.size() == 0)
+            return 0;
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        int last = nums[end];
+        
+        while ( (start + 1) < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= last)
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid;
+            }
+        }
+        
+        if ( nums[start] < nums[end] )
+        {
+            return nums[start];
+        }
+        else
+        {
+            return nums[end];
+        }
+    }
+    
+};
