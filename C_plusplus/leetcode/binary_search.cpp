e048311c7641420794782709a2caffffc502681d
Chi_Liu <chil1@alumni.cmu.edu>
2018-06-04 22:30:25 -0400

    add algorithm Find Peak Element

--- C_plusplus/leetcode/binary_search.cpp
+++ C_plusplus/leetcode/binary_search.cpp
@@ -93,6 +93,55 @@ public:
     
     }
     
+ 
+public:
+    // 75. Find Peak Element
+    // There is an integer array which has the following features:
+
+    // The numbers in adjacent positions are different.
+    // A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+    // We define a position P is a peak if:
+
+    // A[P] > A[P-1] && A[P] > A[P+1]
+    // Find a peak element in this array. Return the index of the peak.
+
+    // Example
+    // Given [1, 2, 1, 3, 4, 5, 7, 6]
+
+    // Return index 1 (which is number 2) or 6 (which is number 7)
+
+    // Challenge
+    // Time complexity O(logN)
+    /*
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    int findPeak(vector<int>& A) {
+        // write your code here
+        if ( A.size() == 0 )
+        {
+            return 0;
+        }
+        
+        int start = 1;
+        int end = A.size() - 1;
+        
+        while ( start + 1 < end )
+        {
+            int mid = start + (end - start) / 2;
+            if ( A[mid] > A[mid-1] && A[mid] > A[mid+1] )
+            {
+                return mid;
+            }
+            else if (A[mid] > A[mid-1])
+            {
+                start = mid;
+            }else if (A[mid] < A[mid-1])
+            {
+                end = mid;
+            }
+        }
+    }    
     
     
 };
