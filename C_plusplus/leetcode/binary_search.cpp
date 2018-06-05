2b6ef9d069ac5e106766a7a718fcc4803383fc85
Chi_Liu <chil1@alumni.cmu.edu>
2018-06-05 07:10:42 -0400

    add algorithm 62. Search in Rotated Sorted Array

--- C_plusplus/leetcode/binary_search.cpp
+++ C_plusplus/leetcode/binary_search.cpp
@@ -143,5 +143,80 @@ public:
         }
     }    
     
+
+public:
+    // 62. Search in Rotated Sorted Array
+    // Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+    // (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+    // You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+    // You may assume no duplicate exists in the array.
+
+    // Example
+    // For [4, 5, 1, 2, 3] and target=1, return 2.
+
+    // For [4, 5, 1, 2, 3] and target=0, return -1.
+
+    // Challenge
+    // O(logN) time    
+    /**
+     * @param A: an integer rotated sorted array
+     * @param target: an integer to be searched
+     * @return: an integer
+     */
+    int search(vector<int> &A, int target) {
+        // write your code here
+        if ( A.size() == 0)
+        {
+            return -1;
+        }
+        
+        int start = 0;
+        int end = A.size() - 1;
+        
+        while ( start + 1 < end )
+        {
+            int mid = start + (end - start) / 2;
+            if ( A[mid] == target )
+            {
+                return mid;
+            }
+            
+            if ( A[start] < A[mid])
+            {
+                if( target >= A[start] && target < A[mid] )
+                {
+                    end = mid;
+                }else
+                {
+                    start = mid;
+                }
+            }else
+            {
+                if ( target >= A[mid] && target <= A[end] )
+                {
+                    start = mid;
+                }else
+                {
+                    end = mid;
+                }
+            }
+            
+        }
+        
+        if ( A[start] == target )
+        {
+            return start;
+        }
+        if( A[end] == target )
+        {
+            return end;
+        }
+        
+        return -1;
+    }    
+    
     
 };
