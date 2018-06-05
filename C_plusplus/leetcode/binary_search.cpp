cd1b77fb48c547110553086f5fa69c10a910e52d
Chi_Liu <chil1@alumni.cmu.edu>
2018-06-04 20:15:07 -0400

    add algorithm Maximum Number in Mountain Sequence

--- C_plusplus/leetcode/binary_search.cpp
+++ C_plusplus/leetcode/binary_search.cpp
@@ -47,4 +47,52 @@ public:
         }
     }
     
+    
+public:
+    
+    // 585. Maximum Number in Mountain Sequence
+    // Given a mountain sequence of n integers which increase firstly and then decrease, find the mountain top.
+
+    // Example
+    // Given nums = [1, 2, 4, 8, 6, 3] return 8
+    // Given nums = [10, 9, 8, 7], return 10
+    /**
+     * @param nums: a mountain sequence which increase firstly and then decrease
+     * @return: then mountain top
+     */
+    int mountainSequence(vector<int> &nums) {
+        // write your code here
+        if ( nums.size() == 0 )
+        {
+            return 0;
+        }
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while ( start + 1 < end)
+        {
+            int mid = start + (end - start) / 2;
+            if ( nums[mid] > nums[mid+1] )
+            {
+                end = mid;
+            }
+            else if ( nums[mid] < nums[mid+1] )
+            {
+                start = mid;
+            }
+        }
+        
+        if (nums[start] > nums[end])
+        {
+            return nums[start];
+        }else
+        {
+            return nums[end];
+        }
+    
+    }
+    
+    
+    
 };
